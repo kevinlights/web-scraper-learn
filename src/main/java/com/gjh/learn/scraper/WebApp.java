89de86627f800932440fb36afeac61deaf4ff537
guangjh <guangjinhai@163.com>
2021-01-10 20:45:52 +0800

    Selenium window and others feature test

--- src/main/java/com/gjh/learn/scraper/WebApp.java
+++ src/main/java/com/gjh/learn/scraper/WebApp.java
@@ -1,4 +1,11 @@
 package com.gjh.learn.scraper;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
 }
--- src/test/java/com/gjh/learn/scraper/selenium/HelloSeleniumTest.java
+++ src/test/java/com/gjh/learn/scraper/selenium/HelloSeleniumTest.java
@@ -3,17 +3,24 @@ package com.gjh.learn.scraper.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class HelloSeleniumTest {
     private static final Logger LOG = LoggerFactory.getLogger(HelloSeleniumTest.class);
@@ -25,6 +32,7 @@ public class HelloSeleniumTest {
     public void setUp() throws Exception {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 5);
+        LOG.info("set up driver and wait");
     }
 
     @Test
@@ -87,6 +95,76 @@ public class HelloSeleniumTest {
         driver.switchTo().window(originalWindow);
         LOG.info("switch to origin window");
 
+    }
+
+    @Test
+    public void testIframe() {
+        driver.navigate().to("http://127.0.0.1:8080/iframe.html");
+        // WebElement iframe = driver.findElement(By.cssSelector("#modal>iframe"));
+        // driver.switchTo().frame(iframe);
+
+        // driver.switchTo().frame("buttonframe");
+        // driver.switchTo().frame("myframe");
+        driver.switchTo().frame(0);
+        driver.findElement(By.tagName("button")).click();
+
+        driver.switchTo().defaultContent();
+    }
+
+    @Test
+    public void testWindow() throws InterruptedException {
+        driver.manage().window().setSize(new Dimension(1024, 768));
+        LOG.info("width={}", driver.manage().window().getSize().getWidth());
+        LOG.info("height={}", driver.manage().window().getSize().getHeight());
+
+        driver.manage().window().setPosition(new Point(0, 0));
+        Point position = driver.manage().window().getPosition();
+        LOG.info("x={}", position.getX());
+        LOG.info("y={}", position.getY());
+
+        driver.manage().window().maximize();
+
+        driver.manage().window().fullscreen();
+
+
+    }
+
+    @Test
+    public void testScreenshot() throws IOException {
+        driver.navigate().to("https://www.baidu.com/");
+        driver.manage().window().maximize();
+        String screenshotBase64 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+        LOG.info("screenshot={}", screenshotBase64);
+
+        WebElement hot = driver.findElement(By.cssSelector("div.s_form_wrapper"));
+        File sc = hot.getScreenshotAs(OutputType.FILE);
+        FileCopyUtils.copy(sc, new File("./build/tmp/baidu_hot.png"));
+    }
+
+    @Test
+    public void testScript() {
+        driver.navigate().to("https://movie.douban.com/top250");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement button = driver.findElement(By.cssSelector("div.inp-btn input"));
+        js.executeScript("arguments[0].click();", button);
+        // js.executeScript("return arguments[0].innerText", button);
+        js.executeScript("console.log('hello world')");
+    }
+
+    @Test
+    public void testWait() {
+        driver.navigate().to("http://127.0.0.1:8080/wait.html");
+        WebElement firstResult = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a/h3")));
+
+        WebElement foo = wait.until(d -> d.findElement(By.name("q")));
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        Wait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+        fluentWait.until(webDriver -> webDriver.findElement(By.id("foo")));
     }
 
     @After
